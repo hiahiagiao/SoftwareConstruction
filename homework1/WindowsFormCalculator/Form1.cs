62db8191feeb39235d3bdecac57a6d477978200b
WangChu <88271179+hiahiagiao@users.noreply.github.com>
2022-02-27 08:34:36 +0800

    Add files via upload

---
+++ homework1/WindowsFormCalculator/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(textBox1.Text);
+            double num2 = double.Parse(textBox2.Text);
+            string op = comboBox1.Text;
+            double res = 0;
+            switch (op)
+            {
+                case "+":
+                    res = num1 + num2;
+                    break;
+                case "-":
+                    res = num1 - num2;
+                    break;
+                case "*":
+                    res = num1 * num2;
+                    break;
+                case "/":
+                    res = num1 / num2;
+                    break;
+            }
+            label1.Text = res.ToString();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ homework1/calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace homework1
+{
+    class calculator
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("input number1");
+            string numb1 = Console.ReadLine();
+            double num1 = double.Parse(numb1);
+
+            Console.WriteLine("input number2");
+            string numb2 = Console.ReadLine();
+            double num2 = double.Parse(numb2);
+
+            Console.WriteLine("operation");
+            string op = Console.ReadLine();
+
+            Console.WriteLine(calculate(op, num1, num2));
+
+            static double calculate(string op, double num1, double num2)
+            {
+                double res = 0;
+                switch (op)
+                {
+                    case "+":
+                        res = num1 + num2;
+                        break;
+                    case "-":
+                        res = num1 - num2;
+                        break;
+                    case "*":
+                        res = num1 * num2;
+                        break;
+                    case "/":
+                        res = num1 / num2;
+                        break;
+                }
+                return res;
+            }
+        }
+    }
+}
